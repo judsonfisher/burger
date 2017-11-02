ffd6189ad92ef1cdd464ffb7b1e90aafa6f12f11
Your Namejudsonfisher <judsonefisher@gmail.com>
2017-11-02 13:51:54 -0500

    progress

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burger_db;
+
+INSERT INTO burgers (burger_name)
+VALUES ("Quarter Pounder");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Cheeseburger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Veggie Burger");
--- server.js
+++ server.js
@@ -1,3 +1,20 @@
 var express = require('express');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+
+var app = express();
+var port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require('./controllers/burgers_controllers.js');
+app.use('/', routes);
+
+app.listen(port, function() {
+  console.log("listening on port", port);
+});
